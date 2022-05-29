3fce6c48fcaa13446326b7479f46bc8be3e34007
Ildi Dvorani <dvoranii@ildidev.com>
2022-05-29 16:01:32 -0400

    improved lighthouse performance by compressing images

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,5 @@
+// PROJECTS PAGE
+
 let html2;
 let list_items2 = [];
 
@@ -102,3 +104,33 @@ function paginate(listItem, container) {
 // Should be showing associated services, might need to do this in php
 
 getProjects("/models/projects-model.json");
+
+function getServicesSidebar(url) {
+  let container = document.querySelector(".sidebar-services");
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let arr = Object.entries(data.Services);
+
+      let count = 0;
+      arr.forEach((service) => {
+        count++;
+        sidebarTemplate = `
+          <h3 class="heading-tertiary">${service[1].name}</h3>
+          <a href='${service[1].url}'><img class="service-img" src="/img/${service[1].img}" alt="" /></a>
+          <p>
+           ${service[1].description}
+          </p>
+          <br />
+        `;
+        //   Only show first 3
+        if (count <= 3) {
+          container.innerHTML += sidebarTemplate;
+        }
+      });
+    });
+}
+
+getServicesSidebar("/models/services-model.json");
